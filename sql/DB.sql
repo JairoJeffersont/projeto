89819648f38abbb55981f8c2c1859b1c07e8e4d2
Jairo Jefferson Teixeira Dos Santos <jairojeffersont@gmail.com>
2025-10-09 09:01:07 -0300

    v1

--- sql/DB.sql
+++ sql/DB.sql
@@ -68,3 +68,39 @@
         usuario (id, nome, email, senha, telefone, gabinete_id, tipo_usuario_id) 
     VALUES 
         ('1', 'Usuário Sistema', 'email@email.com', '$2y$10$e0NRG7k8bq5lYFh3H8jzUuJ8mZ6kF1O9H1Z1Z1Z1Z1Z1Z1Z1Z1Z1', '00000000000', '1', '1');
+
+    CREATE TABLE tipo_orgao (
+        id VARCHAR(36) PRIMARY KEY,
+        nome VARCHAR(50) NOT NULL UNIQUE,
+        gabinete_id VARCHAR(36) NOT NULL,
+        FOREIGN KEY (gabinete_id) REFERENCES gabinete (id) ON DELETE RESTRICT ON UPDATE CASCADE,
+        created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+
+    INSERT INTO
+        tipo_orgao (id, nome, gabinete_id)
+    VALUES
+        ('1', 'Sem tipo definido', '1');
+
+
+    CREATE TABLE orgaos(
+        id VARCHAR(36) PRIMARY KEY,
+        nome VARCHAR(100) NOT NULL,
+        email VARCHAR(100) NOT NULL,
+        telefone VARCHAR(20) NOT NULL,
+        endereco VARCHAR(255) NOT NULL,
+        cidade VARCHAR(100) NOT NULL,
+        estado VARCHAR(2) NOT NULL,
+        site VARCHAR(100) DEFAULT NULL,
+        instagram VARCHAR(100) DEFAULT NULL,
+        facebook VARCHAR(100) DEFAULT NULL,
+        twitter VARCHAR(100) DEFAULT NULL,
+        informacoes_adicionais TEXT DEFAULT NULL,
+        tipo_id VARCHAR(36) NOT NULL,
+        FOREIGN KEY (tipo_id) REFERENCES tipo_orgao (id) ON DELETE RESTRICT ON UPDATE CASCADE,
+        gabinete_id VARCHAR(36) NOT NULL,
+        FOREIGN KEY (gabinete_id) REFERENCES gabinete (id) ON DELETE RESTRICT ON UPDATE CASCADE,
+        created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP        
+    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
